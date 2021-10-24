9405e261e9d847f823782de5767908304dc60679
dimassusyanto <dimassusyanto12@gmail.com>
2021-10-24 15:39:25 +0700

    pesan

--- belajar/index.py
+++ belajar/index.py
@@ -6,6 +6,9 @@ print("nama saya dimas")
 print("hello")  # ini juga komentar
 
 # membuat variable (string)
-syntaxcommit = "git -c user.email="your@email.com" -c user.name="Your Name" commit ..."
-
+# git
+# git status
+# git add .
+# git commit ( "git -c user.email="dimassusyanto12@gmail.com" -c user.name="dimassusyanto" commit -m ('pesan'))
+# git push
 # tipe data
